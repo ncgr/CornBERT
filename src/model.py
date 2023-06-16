15f4e1fa8957e34742407f24270723be1ec756f1
danielsalinasduron <d.salinasduron@msmary.edu>
2023-06-16 09:16:17 -0600

    apply mean across correct dimension and add regression head in model.py

--- src/model.py
+++ src/model.py
@@ -7,14 +7,18 @@ class cornBERT(nn.Module):
     def __init__(self):
         super().__init__()
 
-	# make roberta encoder
+	    # make roberta encoder
         # set max input length to 256
         configuration = RobertaConfig(max_position_embeddings=256)
         self.encoder = RobertaModel(configuration)
+        self.rh_L1 = nn.Linear(768,10) # regression head linear layer 1
+        self.rh_L2 = nn.Linear(10,10) # regression head linear layer 2
 
     def forward(self,x):
         x = self.encoder(**x)
-        x = torch.mean(x.last_hidden_state,dim=2)
+        x = torch.mean(x.last_hidden_state,dim=1) # avg token embeddings
+        x = self.rh_L1(x)
+        x = self.rh_L2(x)
         return x
 
 # test that the model is functional
@@ -22,7 +26,7 @@ class cornBERT(nn.Module):
 # generate random input
 from random import choice
 
-promoter  = [choice("ACTGN") for i in range(100)]
+promoter  = "".join([choice("ACTGN") for i in range(256)])
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 inputs    = tokenizer(promoter, return_tensors="pt")
 
