a21c89ae49087cff59ec10bddba794f6645cb12b
danielsalinasduron <d.salinasduron@msmary.edu>
2023-06-22 15:27:36 -0400

    update roberta config to make model smaller/closer to cornbert paper

--- src/model.py
+++ src/model.py
@@ -3,22 +3,26 @@ import torch.nn as nn
 
 from transformers import RobertaConfig, RobertaModel, AutoTokenizer
 
-class cornBERT(nn.Module):
+class CornBERT(nn.Module):
     def __init__(self):
         super().__init__()
 
-	    # make roberta encoder
+        # set encoder parameters
+        mpe = 256 # maximum sequence length to be fed as a whole to encoder
+        nah = 6 # number of attention heads in the encoder
+        vs  = 5004 # number of tokens in the vocabulary
+        nhl = 2 # number of feed forward layers in each attention head
+
+        # make roberta encoder
         # set max input length to 256
-        configuration = RobertaConfig(max_position_embeddings=256)
+        configuration = RobertaConfig(max_position_embeddings=mpe,
+                                      num_attention_heads=nah,
+                                      num_hidden_layers=nhl)
         self.encoder = RobertaModel(configuration)
-        self.rh_L1 = nn.Linear(768,10) # regression head linear layer 1
-        self.rh_L2 = nn.Linear(10,10) # regression head linear layer 2
 
     def forward(self,x):
         x = self.encoder(**x)
         x = torch.mean(x.last_hidden_state,dim=1) # avg token embeddings
-        x = self.rh_L1(x)
-        x = self.rh_L2(x)
         return x
 
 # test that the model is functional
@@ -31,6 +35,6 @@ tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 inputs    = tokenizer(promoter, return_tensors="pt")
 
 # run the model on the random input
-cbt = cornBERT()
+cbt = CornBERT()
 out = cbt(inputs)
 print(out.size())
