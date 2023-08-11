c261bdb477676bf084b0fda61eefce9051e1fa5f
root <d.salinasduron@msmary.edu>
2023-08-11 18:46:18 +0000

    refactor model.py to include a model for pretraining vs regression task

--- src/model.py
+++ src/model.py
@@ -1,45 +1,50 @@
 import torch
-import torch.nn as nn
 
-from transformers import RobertaForMaskedLM
-from transformers import RobertaConfig, RobertaModel, AutoTokenizer
+from transformers import RobertaConfig, RobertaForMaskedLM, RobertaForSequenceClassification
 
-
-class CornBERT(RobertaForMaskedLM):
-    def __init__(self):
-
-
-        # make roberta for masked training
-        config = RobertaConfig(
+# mixin with config from original study
+class CornBERT:
+    def roberta_config(self):
+        return RobertaConfig(
             vocab_size=5004, # number of tokens in the vocabulary
+            hidden_size = 768, # embedding dimensionality, used throughout the model
             max_position_embeddings=256, # maximum sequence length to be fed as a whole to encoder
             num_attention_heads=6, # number of attention heads in the encoder
-            num_hidden_layers=2, # number of feed forward layers in each attention head
-            type_vocab_size=1,
+            num_hidden_layers=6, # num of encoder layers
+            type_vocab_size=1, # num of token types, in next sentence prediction tasks, one per sentence
+            num_labels=10, # number of plant tissues. used in sequence classification model, not used in maskedlm model
         )
-        # roberta accessible through self.roberta
-        # combplete list of submodules accessible through self.named_modules
-        super().__init__(config=config)
-    #def forward(self,x):
-    #    return self.roberta(x)
 
+# child inherits data attributes from leftmost parent class, e.g. will inherit the model
+class CornBERTforMaskedLM(RobertaForMaskedLM,CornBERT):
+    def __init__(self):
+        super().__init__(config=self.roberta_config())
+
+class CornBERTforGeneExpression(RobertaForSequenceClassification,CornBERT):
+    def __init__(self):
+        super().__init__(config=self.roberta_config())
+
+    # similar to RobertaForSequenceClassification, but 
+    """
+    Modified to take the average. DOES NOT FILTER OUT PADDING. You must do this in preprocessing.
+    """
+    def forward(self,x):
+        x = self.roberta(x)
+        # average embedding of all tokens instead of only taking embedding of first
+        x = torch.mean(x.last_hidden_state,1,keepdim=True) # average over the second dimension but do not remove it
+        x = self.classifier(x)
+        return x
 
 # test that the model is functional
 if __name__ == '__main__':
 
-    # generate random input
-    from random import choice
-
-    f = open("training/yeast/y12_promoters")
-    promoter  = f.readline()
-    f.close()
-    global tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-    global inputs
-    inputs    = tokenizer(promoter, return_tensors="pt")
-
-    # run the model on the random input
-    global cbt
-    cbt = CornBERT()
-    #global out
-    #out = cbt(inputs.input_ids)
+    # test on random input
+    from torch import randint
+    global model
+    model = CornBERTforMaskedLM()
+    #model = CornBERTforGeneExpression()
+    tokens = torch.randint(low=100,high=5000,size=(1,250))
+    tokens[0][0] = 0
+    tokens[0][-1] = 2
+    global output
+    output = model(tokens)
